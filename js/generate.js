12a1d08e36b10a1431aeb69b37f544aacf23c2d6
pppushinw <101379867+pppushinw@users.noreply.github.com>
2022-11-16 09:43:24 +0300

    1

---
+++ js/generate.js
@@ -0,0 +1,5 @@
+const cardTemplate = document.querySelector('#card');
+const cardElement = document.querySelector('.popup');
+const cardChildrens = cardElement.children;
+
+console.log(cardChildrens)
