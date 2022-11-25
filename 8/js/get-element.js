b5e95e93faa4ac45a7805a5edd7daa230db3991d
Keks <keks@htmlacademy.ru>
2022-11-25 04:35:48 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/get-element.js
@@ -0,0 +1,47 @@
+import {features} from './util.js';
+import {photos} from './util.js';
+import {getRandomPositiveInteger} from './get-random-number.js';
+
+export function getFeatures() {
+  const maxLength = features.length;
+  const lengthOfArray = getRandomPositiveInteger(1, maxLength);
+  const array = [];
+
+  while (array.length < lengthOfArray) {
+    const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
+    const el = features[indexOfEl];
+
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+}
+
+export function getPhotos() {
+  const maxLength = photos.length;
+  const lengthOfArray = getRandomPositiveInteger(1, maxLength);
+  const array = [];
+
+  while (array.length < lengthOfArray) {
+    const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
+    const el = photos[indexOfEl];
+
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+}
+
+export function getAvatar(id) {
+  let numberPhoto = id.toString();
+  let linkAvatar = `img/avatars/user${'0' + numberPhoto}.png`;
+  if (id === 0) {
+    linkAvatar = `img/avatars/user${numberPhoto}.png`
+  }
+  if (id >= 10) {
+    linkAvatar = `img/avatars/user${numberPhoto}.png`
+  }
+  return linkAvatar;
+}
---
+++ 8/js/map.js
@@ -0,0 +1,47 @@
+import { cardArray } from "./generate.js";
+
+export const map = L.map('map-canvas')
+  .on('load', () => {
+    console.log('Карта готова')
+  })
+  .setView({
+    lat: 35.6895,
+    lng: 139.69171,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainMarker = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+export const defaultMarkerIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+})
+
+const marker = L.marker(
+  {
+    lat: 35.6895,
+    lng: 139.69171,
+  },
+  {
+    draggable: true,
+    icon: mainMarker
+  },
+);
+
+marker.addTo(map)
+
+export const inputAddress = document.querySelector('#address')
+marker.on('moveend', (evt) => {
+  inputAddress.value = evt.target.getLatLng()
+})
